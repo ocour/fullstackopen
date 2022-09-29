d5e16eb0b08e8b4c07647ac89afb9088ef204812
ocourt2 <ocourt@edu.lapinamk.fi>
2022-09-29 15:28:53 +0300

    part3/phonebook

---
+++ part3-phonebook/index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// root
+app.get('/', (request, response) => {
+    response.send('<h1>Root</h1>');
+})
+
+// gets all persons
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+})
+
+// gets info
+app.get('/info', (request, response) => {
+    const info = `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`;
+    response.send(info);
+})
+
+// gets one person by id, gives 404 if not person with give id exists
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if(person)
+    {
+        return response.json(person);
+    }
+    else
+    {
+        return response.status(404).end();
+    }
+})
+
+// deletes single person
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    response.status(204).end();
+
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    // Checks if person with name already exists
+    // case insensitive and whitespace insensitive
+    const existingPerson = persons.some(person => 
+        person.name.toLowerCase().replace(/\s/g,"") === body.name.toLowerCase().replace(/\s/g,""));
+
+    // if given name or number is missing or empty string ""
+    // return status code 400 and error message in json
+    if(!body.name || !body.number)
+    {
+        return response.status(400).json({
+            error: "name or body missing"
+        })
+    }
+    // returns error message if person with given name exists
+    else if (existingPerson)
+    {
+        return response.status(400).json({
+            error: "person with name already exists"
+        })
+    }
+
+    const person = {
+        id: Math.floor(Math.random() * 100000),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person);
+
+    response.json(person);
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
