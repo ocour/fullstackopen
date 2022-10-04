7355842d9ca53229e026253f4e6be98d303ffa49
ocourt2 <ocourt@edu.lapinamk.fi>
2022-10-04 11:33:12 +0300

    part3/c/done

--- part3-phonebook-backend/index.js
+++ part3-phonebook-backend/index.js
@@ -30,36 +30,45 @@ app.get('/api/persons', (request, response) => {
 })
 
 // gets info
-app.get('/info', (request, response) => {
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`;
-    response.send(info);
+app.get('/info', (request, response, next) => {
+    Person.countDocuments()
+        .then(length => {
+            const info = `<p>Phonebook has info for ${length} people</p>
+            <p>${new Date()}</p>`;
+            response.send(info);
+        })
+        .catch(error => next(error));
 })
 
 // gets one person by id, gives 404 if not person with give id exists
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(person => person.id === id);
-
-    if(person)
-    {
-        return response.json(person);
-    }
-    else
-    {
-        return response.status(404).end();
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            // if not null
+            if(person)
+            {
+                response.json(person);
+            }
+            else
+            {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 })
 
 // deletes single person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            console.log(result);
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 
 })
 
+// When adding new person
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
@@ -84,11 +93,42 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+// update by id, for example updating phone number
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    // {new:true} is an option that gives back updated person
+    // otherwise the old person would be given back
+    Person.findByIdAndUpdate(request.params.id, person, {new:true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error));
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" });
 }
 
 app.use(unknownEndpoint);
+
+// Error handling
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+
+    if(error.name === "CastError")
+    {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    next(error);
+}
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
