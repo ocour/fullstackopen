b11a9df5642257ef4fbc4382c166157fdc3d3ef7
ocourt2 <ocourt@edu.lapinamk.fi>
2022-10-09 12:41:21 +0300

    part4/c

--- part3-notes-backend/controllers/notes.js
+++ part3-notes-backend/controllers/notes.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 const logger = require("../utils/logger");
 
 // get all notes
 notesRouter.get("/", async (request, response) => {
-    const notes = await Note.find({});
+    const notes = await Note
+        .find({})
+        .populate("user", { username: 1, name: 1 });
     response.json(notes);
 });
 
@@ -34,18 +37,26 @@ notesRouter.get("/:id", async (request, response, next) => {
 // Post/add note(s)
 notesRouter.post("/", async (request, response, next) => {
     const body = request.body;
-    logger.info(body);
+
+    const user = await User.findById(body.userId);
 
     // Create note
     const note = new Note({
         content: body.content,
-        important: body.important || false,
+        important: body.important === undefined ? false : body.important,
         date: new Date(),
+        user: user._id,
     });
 
-    //add note
+    //add note and note id to users
     try {
+        // saves note
         const savedNote = await note.save();
+
+        // adds savedNotes id to users notes array and saves it
+        user.notes = user.notes.concat(savedNote._id);
+        await user.save();
+
         response.status(201).json(savedNote);
     }
     // if error
--- part3-notes-backend/controllers/users.js
+++ part3-notes-backend/controllers/users.js
@@ -4,7 +4,10 @@ const User = require("../models/user");
 
 usersRouter.get("/", async (request, response, next) => {
     try {
-        const users = await User.find({});
+        const users = await User
+            .find({})
+            .populate("notes", { content: 1, date: 1 });
+
         response.json(users);
     }
     catch(exception) {
